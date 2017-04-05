abb346ecf033bf48e542c8a48e64594a86c439ab
Alex Kiura <kiuraalex@gmail.com>
2017-04-05 22:52:21 +0300

    feat: add application routes

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@
 
 Route::auth();
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');
 
 Auth::routes();
 
